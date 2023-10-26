3e9a4a2a54c20cce4f6f91a196b34f5f2d10fa6b
brunonlima <brunofatecscs@gmail.com>
2023-10-25 21:14:52 -0300

    day04 ex00 - refatorando

---
+++ day04/ex/ex00/src/form/NeedMoreClassRoomForm.hpp
@@ -0,0 +1,14 @@
+// NeedMoreClassRoomForm.hpp
+#ifndef NEEDMORECLASSROOMFORM_HPP
+#define NEEDMORECLASSROOMFORM_HPP
+
+#include "Form.hpp"
+
+class NeedMoreClassRoomForm : public Form {
+private:
+
+public:
+    void execute();
+};
+
+#endif // NEEDMORECLASSROOMFORM_HPP
---
+++ day04/ex/ex00/src/person/Student.hpp
@@ -0,0 +1,21 @@
+// Student.hpp
+#ifndef STUDENT_HPP
+#define STUDENT_HPP
+
+#include "Person.hpp"
+#include <vector>
+
+class Course;
+class Classroom;
+
+class Student : public Person {
+private:
+    std::vector<Course*> _subscribedCourse;
+
+public:
+    void attendClass(Classroom* p_classroom);
+    void exitClass();
+    void graduate(Course* p_course);
+};
+
+#endif // STUDENT_HPP
